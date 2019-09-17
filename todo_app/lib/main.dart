c85098ce6d09a6e00fa29bcdcb5bc27527515431
batman1291999 <47320366+srikrishna1999@users.noreply.github.com>
2019-09-18 00:29:56 +0530

    Add files via upload

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/ui/home.dart';
+
+void main() async {
+      runApp(MaterialApp(
+        title: "Todo App",
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ));
+    }
